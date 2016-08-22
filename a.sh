0145e9da6c7948b77f00d883edcfcebacbb18f91
andras-kiss <akiss@gamma.ttk.pte.hu>
2016-08-22 20:04:10 +0200

    Prepared all the electrode characterization tikz plots.

--- a.sh
+++ a.sh
@@ -11,8 +11,8 @@
 #gnuplot plot_transient
 
 # gnuplot plot_sb
-pdflatex -shell-escape phd.tex
+#pdflatex -shell-escape phd.tex
 bibtex phd
 pdflatex -shell-escape phd.tex
-pdflatex -shell-escape phd.tex
-rm *.{aux,bbl,blg,lof,log,lot,out,toc}
+#pdflatex -shell-escape phd.tex
+#rm *.{aux,bbl,blg,lof,log,lot,out,toc}
